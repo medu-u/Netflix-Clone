47141e0ce2a1f41981892d62d5b95e6ceb110546
Medi Mule <medhanitmulatu6796@gmail.com>
2025-12-09 22:39:45 +0300

    favicon changed

--- Netflix-Clone-2025/src/utils/axios.jsx
+++ Netflix-Clone-2025/src/utils/axios.jsx
@@ -4,4 +4,3 @@ const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
 export default instance;
-// https://api.themoviedb.org/3/discover/tv?api_key=72bb4ca42ec132a01bd72505f8faebe4&with_networks=213
